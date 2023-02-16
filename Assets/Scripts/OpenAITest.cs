e22e784a9b2b7ff4e650d2372cda6bddb3aa754f
jstokes <joel.stokes.e@gmail.com>
2023-02-16 17:45:57 -0500

    Key correction & 1st Test

--- Assets/Scripts/OpenAITest.cs
+++ Assets/Scripts/OpenAITest.cs
@@ -6,19 +6,34 @@ using System.Threading.Tasks;
 
 public class OpenAITest : MonoBehaviour
 {
+    public string userInput;
+
     public void PressButton(){
         SetOpenAI();
     }
 
-    private async void SetOpenAI(){
-        //var api = new OpenAIClient(OpenAIAuthentication.LoadFromDirectory("your/path/to/.openai"));;
-
+    public async void SetOpenAI(){
         var api = new OpenAIClient();
         var model = await api.ModelsEndpoint.GetModelDetailsAsync("text-davinci-003");
         Debug.Log(model.ToString());
 
-        var result = await api.CompletionsEndpoint.CreateCompletionAsync("Create an outline for an essay about jelly beans:", 
+        //Start "Loading" Icon
+
+        var result = await api.CompletionsEndpoint.CreateCompletionAsync("Create an outline for an essay about " + userInput + " without an introduction or conclusion:", 
             maxTokens: 150, temperature: 0.3, frequencyPenalty: 0.0, model: model);
         Debug.Log(result);
+
+        //End "Loading"
+    }
+
+    private void FormatData(){
+        //Set Title Node as Center
+
+        //Remove Introduction Section
+
+        //Breakup Headers & Subsections into proper Nodes
+
+        //Remove Conclusion
+
     }
 }
